6840cb6743afca2b2192b3abd7fe2de6e16a7754
jesuarva <jesuarva.developer@gmail.com>
2018-10-27 20:03:22 +0200

    WIRE endpoints.js with server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,9 +1,15 @@
 const server = require('express')();
-const { setUpGlobalMiddlewares } = require('./midlewares').globalMiddlewares;
+const setUpGlobalMiddlewares = require('./midlewares').globalMiddlewares;
+const setUpEndpoints = require('./endpoints');
 
 /**
  * Apply server middleware
  */
 setUpGlobalMiddlewares(server);
+
+/**
+ * Apply routes
+ */
+setUpEndpoints(server);
 
 module.exports = server;
