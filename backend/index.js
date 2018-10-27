889377ad3f93eadc1bfc5d91a593c19c160bd01c
jesuarva <jesuarva.developer@gmail.com>
2018-10-27 20:07:46 +0200

    IMPROVE port definitions for server

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const server = require('./server/server');
 
 const { PORT } = process.env;
+const port = PORT || 2373;
 
-server.listen(PORT, () => {
+server.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`\n*** Listening on port ${PORT} ***\n`);
+  console.log(`\n*** Listening on port ${port} ***\n`);
 });
