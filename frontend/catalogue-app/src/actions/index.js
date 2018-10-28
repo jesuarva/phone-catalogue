ebdfde9a3c0f3627af38a182d9e12e35bbed8af4
jesuarva <jesuarva.developer@gmail.com>
2018-10-28 14:16:18 +0100

    DEFINE actions/index.js

---
+++ frontend/catalogue-app/src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const FETCHING_ITEMS = 'FETCHING_ITEMS';
+export const FETCHED_ITEMS = 'FETCHED_ITEM';
+export const ERROR = 'ERROR';
+
+const URL = process.env.REACT_APP_API_CATALOGUE || '/phones';
+
+const errorAction = (error) => {
+  return {
+    type: ERROR,
+    message: error.message,
+  };
+};
+
+export const fetchingItems = () => {
+  const fetch = axios.get(URL);
+  return (dispatch) => {
+    dispatch({
+      type: FETCHING_ITEMS,
+    });
+    fetch
+      .then((response) => {
+        console.log('response.data', response.data);
+        dispatch({
+          type: FETCHED_ITEMS,
+          data: response.data['Document(s) in database'],
+        });
+      })
+      .catch((e) => {
+        console.log('error', e);
+        dispatch(errorAction(e));
+      });
+  };
+};
