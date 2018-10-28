b95b85e6bcda2186932f11edf9e068a3afb348d2
jesuarva <jesuarva.developer@gmail.com>
2018-10-28 14:30:49 +0100

    WIRE APP's indes.js wiht Redux

--- frontend/catalogue-app/src/index.js
+++ frontend/catalogue-app/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import mainReducer from './reducers';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(mainReducer, applyMiddleware(thunk, logger));
+console.log('process.env', process.env);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
