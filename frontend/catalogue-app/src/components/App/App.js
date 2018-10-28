ace9b57ef1e93dd57eae43dacd5896129bcd0cc8
jesuarva <jesuarva.developer@gmail.com>
2018-10-28 14:55:05 +0100

    DEFINE header in App.js

--- frontend/catalogue-app/src/components/App/App.js
+++ frontend/catalogue-app/src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import { fetchingItems } from '../../actions';
+import MAXMOVIL_LOGO from '../../media/Masmovillogo.svg.png';
 
 class App extends Component {
   componentDidMount() {
@@ -11,8 +12,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
+      <div className="App container">
+        <div className="header row">
+          <h1 className="display-1 col-12">Phone catalogue</h1>
+          <figure className="col-12 figure">
+            <img
+              src={MAXMOVIL_LOGO}
+              alt="masmovil logo"
+              className="figure-img img-fluid rounded"
+            />
+          </figure>
+        </div>
+        <div className="row" />
       </div>
     );
   }
