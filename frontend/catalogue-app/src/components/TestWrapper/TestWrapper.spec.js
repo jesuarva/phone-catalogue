21e310e2105cd82bbbdf1016d06ad11874049c78
jesuarva <jesuarva.developer@gmail.com>
2018-10-29 02:07:32 +0100

    CLEAN code

--- frontend/catalogue-app/src/components/TestWrapper/TestWrapper.spec.js
+++ frontend/catalogue-app/src/components/TestWrapper/TestWrapper.spec.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
 import renderer from 'react-test-renderer';
 import TestWrapper from './TestWrapper';
 
 configure({ adapter: new Adapter() });
 
-/**
- * @author jesuarva
- */
 it('should renders correctly', () => {
   const tree = renderer.create(<TestWrapper>{() => <div />}</TestWrapper>);
   expect(tree).toMatchSnapshot();
